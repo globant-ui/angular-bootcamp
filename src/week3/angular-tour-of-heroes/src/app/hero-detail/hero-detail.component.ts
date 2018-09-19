b070dc97e224b678a4927f92ed57efcd6e8aca3d
DESKTOP-64CCSB0\Juanfri <juanfri.m@hotmail.com>
2018-09-19 13:35:12 -0300

    Tour of Heroes v5

--- src/week3/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ src/week3/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../models/hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
    selector: 'app-hero-detail',
@@ -9,9 +12,24 @@ import { Hero } from '../models/hero';
 export class HeroDetailComponent implements OnInit {
    @Input() hero: Hero;
 
-   constructor() { }
+   constructor(
+   private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+   ) {}
 
-   ngOnInit() {
+   ngOnInit(): void {
+      this.getHero();
+   }
+
+   getHero(): void {
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+   }
+
+   goBack(): void {
+      this.location.back();
    }
 
 }
--- src/week3/angular-tour-of-heroes/src/app/hero.service.ts
+++ src/week3/angular-tour-of-heroes/src/app/hero.service.ts
@@ -13,9 +13,16 @@ import { MessageService } from './message.service';
 export class HeroService {
 
    constructor(private messageService: MessageService) { }
-   
+
    getHeroes(): Observable<Hero[]> {
+      // TODO: send the message _after_ fetching the hero
       this.messageService.add('HeroService: fetched heroes');
       return of(HEROES);
+   }
+
+   getHero(id: number): Observable<Hero> {
+      // TODO: send the message _after_ fetching the hero
+      this.messageService.add(`HeroService: fetched hero id=${id}`);
+      return of(HEROES.find(hero => hero.id === id));
    }
 }
