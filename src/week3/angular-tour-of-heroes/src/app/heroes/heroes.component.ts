0feaa87557c0facc026ff631532c6ab367d2feea
DESKTOP-64CCSB0\Juanfri <juanfri.m@hotmail.com>
2018-09-19 12:46:36 -0300

    Tour of Heroes v4: -Copying CSS

--- src/week3/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ src/week3/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../models/hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
    selector: 'app-heroes',
@@ -8,16 +8,22 @@ import { HEROES } from '../mock-heroes';
    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-   heroes = HEROES;
+   heroes: Hero[];
    selectedHero: Hero;
 
-   constructor() { }
+   constructor(private heroService: HeroService) { }
 
    ngOnInit() {
+      this.getHeroes();
    }
 
    onSelect(hero: Hero): void {
       this.selectedHero = hero;
+   }
+
+   getHeroes(): void {
+      this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
    }
 
 }
