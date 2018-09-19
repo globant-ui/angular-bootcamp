9df9982978c5a09fc3a55ecb24d69e7698a66057
DESKTOP-64CCSB0\Juanfri <juanfri.m@hotmail.com>
2018-09-19 10:56:29 -0300

    Working with Angular 6

--- src/week3/angular-tour-of-heroes/e2e/src/app.e2e-spec.ts
+++ src/week3/angular-tour-of-heroes/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('angular-tour-of-heroes App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('angular-tour-of-heroes App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to angular-tour-of-heroes!');
   });
 });
