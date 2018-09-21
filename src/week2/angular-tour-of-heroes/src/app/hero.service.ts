f3dc3d5f8e788ba08b85466a5d1369f2e7109807
Estanislao Kwiatkowski <estanislao.kwiat@gmail.com>
2018-09-21 14:55:35 -0300

    06 - Routing

--- src/week2/angular-tour-of-heroes/src/app/hero.service.ts
+++ src/week2/angular-tour-of-heroes/src/app/hero.service.ts
@@ -17,4 +17,9 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
--- src/week2/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ src/week2/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -18,9 +18,6 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes  => this.heroes = heroes);
