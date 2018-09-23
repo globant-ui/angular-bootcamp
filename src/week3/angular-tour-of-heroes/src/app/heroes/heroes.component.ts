522faefbd8e99a58aa8e6f3c3b4044a8af88f4ce
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2018-09-23 15:36:43 -0300

    step 1 & 2, hero and hero component

---
+++ src/week3/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  
+  constructor() { }
+  
+  ngOnInit() {
+  }
+
+}
