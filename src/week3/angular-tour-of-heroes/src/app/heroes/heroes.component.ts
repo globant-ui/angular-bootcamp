d4818f6a0b6d86e0e185c3bb005c29ce6e822eb4
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2018-09-23 16:06:56 -0300

    step 3, heroes lsit

--- src/week3/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ src/week3/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
   
+  selectedHero: Hero;
+
   constructor() { }
   
   ngOnInit() {
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
